dd56c020fa682f69f1247f75ec901ff169a47c27
guaaan <juampablo.txt@gmail.com>
2022-01-03 17:08:47 -0300

    enviar

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,13 +1,18 @@
 const canvasSketch = require("canvas-sketch");
-
+const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 const settings = {
   dimensions: [1080, 1080],
 };
 
 //formula para convertir grados a radianes
-const degToRad = (degrees) => {
+/*const degToRad = (degrees) => {
   return (degrees / 180) * Math.PI;
 };
+//función para numero random entre dos numeros
+const randomRange = (min, max) => {
+  return Math.random() * (max - min) + min;
+};*/
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -27,7 +32,7 @@ const sketch = () => {
     const radius = width * 0.3;
 
     for (let i = 0; i < num; i++) {
-      const slice = degToRad(360 / num);
+      const slice = math.degToRad(360 / num);
       const angle = slice * i;
 
       x = cx + radius * Math.sin(angle);
@@ -36,10 +41,23 @@ const sketch = () => {
       context.save();
       context.translate(x, y);
       context.rotate(-angle);
+      context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
 
       context.beginPath();
-      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.rect(-w * 0.5, random.range(0,-h * 0.5), w, h);
       context.fill();
+      context.restore();
+
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
+
+      context.lineWidth = random.range(5,20);
+
+      context.beginPath();
+      context.arc(0, 0, radius * random.range(0.7, 1.3), slice * -0.3, slice * 0.3);
+      context.stroke();
+
       context.restore();
     }
   };
