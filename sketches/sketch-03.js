f9a220bc1a4559ec0f602559fb13ec01934851fd
guaaan <juampablo.txt@gmail.com>
2022-01-04 10:55:25 -0300

    mensaje

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -1,25 +1,35 @@
 const canvasSketch = require("canvas-sketch");
-const random = require('canvas-sketch-util/random');
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 };
+
+const animate = () => {
+  console.log("animating");
+  requestAnimationFrame(animate);
+};
+//animate();
 
 const sketch = ({ context, width, height }) => {
   const agents = [];
 
-  for (let i = 0; i < 40; i++) {
-    const x = random.range(0,width);
-    const y = random.range(0,height);
+  for (let i = 0; i < 50; i++) {
+    const x = random.range(0, width);
+    const y = random.range(0, height);
 
     agents.push(new Agent(x, y));
   }
+  
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    agents.forEach(agent =>{
+    agents.forEach((agent) => {
+      agent.update();
       agent.draw(context);
+      agent.bounce(width, height);
     });
   };
 };
@@ -27,7 +37,7 @@ const sketch = ({ context, width, height }) => {
 canvasSketch(sketch, settings);
 
 //set te location of the agents
-class Point {
+class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -36,15 +46,34 @@ class Point {
 
 class Agent {
   constructor(x, y) {
-    this.pos = new Point(x, y);
-    this.radius = 10;
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = random.range(4, 12);
   }
+
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
+  }
+
+  //moves the dots
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+
   // draw the agent
   draw(context) {
-    context.fillStyle = "black";
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+
+    context.lineWidth = 4;
 
     context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
+    context.stroke();
+
+    context.restore();
   }
 }
